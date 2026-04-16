a4dd58732592673c711b42288620a34cd54a72fe
Checkboxer <checkboxer@friedlemons.org>
2026-04-16 08:40:21 +0530

    DEVOPS-399: Refactor DynamoDB resource to use key_schema blocks
    Update the DynamoDB table and global secondary index configurations to use nested key_schema blocks instead of top-level hash_key and range_key arguments. This change uses dynamic blocks to handle optional range keys, ensuring compatibility with newer versions of the AWS provider.

--- main.tf
+++ main.tf
@@ -1,19 +1,33 @@
 resource "aws_dynamodb_table" "main" {
   name             = "${var.project}-${var.environment}-${var.name}"
   billing_mode     = "PAY_PER_REQUEST"
-  hash_key         = var.hash_key
-  range_key        = var.range_key
   stream_enabled   = var.enable_stream
   stream_view_type = var.stream_view_type
+
+  key_schema {
+    attribute_name = var.hash_key
+    key_type       = "HASH"
+  }
+
+  dynamic "key_schema" {
+    for_each = var.range_key != null ? [var.range_key] : []
+    content {
+      attribute_name = key_schema.value
+      key_type       = "RANGE"
+    }
+  }
 
   attribute {
     name = var.hash_key
     type = var.hash_key_type
   }
 
-  attribute {
-    name = var.range_key
-    type = var.range_key_type
+  dynamic "attribute" {
+    for_each = var.range_key != null ? [{ name = var.range_key, type = var.range_key_type }] : []
+    content {
+      name = attribute.value.name
+      type = attribute.value.type
+    }
   }
 
   dynamic "attribute" {
@@ -34,13 +48,23 @@ resource "aws_dynamodb_table" "main" {
 
     content {
       name               = global_secondary_index.value.name
-      hash_key           = global_secondary_index.value.hash_key
       projection_type    = global_secondary_index.value.projection_type
-      range_key          = lookup(global_secondary_index.value, "range_key", null)
       read_capacity      = lookup(global_secondary_index.value, "read_capacity", null)
       write_capacity     = lookup(global_secondary_index.value, "write_capacity", null)
       non_key_attributes = lookup(global_secondary_index.value, "non_key_attributes", null)
+
+      key_schema {
+        attribute_name = global_secondary_index.value.hash_key
+        key_type       = "HASH"
+      }
+
+      dynamic "key_schema" {
+        for_each = lookup(global_secondary_index.value, "range_key", null) != null ? [global_secondary_index.value.range_key] : []
+        content {
+          attribute_name = key_schema.value
+          key_type       = "RANGE"
+        }
+      }
     }
   }
 }
-
